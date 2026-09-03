d4c93c14f8eb309101ef073afaa673cbd9eb13ab
islam ashraf <islamashraf520@gmail.com>
2026-09-03 13:56:34 +0300

    added some changes

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    allUsers() {
+        return "this is coming from user service";
+    }
+}
